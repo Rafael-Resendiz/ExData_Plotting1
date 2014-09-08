c304325d3db17ee312f51f7cefe13cd23c3b88e7
Rafael Resendiz Ramirez <rafaresend@gmail.com>
2014-09-07 20:06:43 -0700

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -1,32 +1,28 @@
-source("load_data.R")
-png(filename = "plot4.png", 
-    width = 480, height = 480,
-    units = "px", bg = "transparent")
-par(mfrow = c(2, 2))
-## Top-left
-plot(DateTime, Global_active_power, 
-     type = "l",
-     xlab = "", ylab = "Global Active Power")
-## Top-right
-plot(DateTime, Voltage,
-     type = "l",
-     xlab = "datetime", ylab = "Voltage")
-## Bottom-left
-plot(DateTime, Sub_metering_1, 
-     type = "l",
-     col = "black",
-     xlab = "", ylab = "Energy sub metering")
-lines(DateTime, Sub_metering_2, col = "red")
-lines(DateTime, Sub_metering_3, col = "blue")
-# Remove the border of legend here.
-legend("topright", 
-       bty = "n",
-       col = c("black", "red", "blue"),
-       c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
-       lwd = 1)
-## Bottom-right
-plot(DateTime, Global_reactive_power, 
-     type = "l",
-     col = "black",
-     xlab = "datetime", ylab = colnames(newData)[4])
+## Plot 4
+source("general_ploting.R")
+par(mfrow=c(2,2), mar=c(4,4,2,2), oma=c(0,0,2,0))
+with(eda2, {
+plot(Global_active_power~Datetime, type="l", 
+    ylab="Global Active Power", xlab="")
+plot(Voltage~Datetime, type="l", 
+    ylab="Voltage", 
+    xlab="datetime")
+plot(Sub_metering_1~Datetime, type="l", 
+    ylab="Energy sub metering", xlab="")
+  lines(Sub_metering_2~Datetime,col='Red')
+  lines(Sub_metering_3~Datetime,col='Blue')
+  legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, bty="n",
+    legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+plot(Global_reactive_power~Datetime, type="l", 
+    ylab="Global Reactive Power",
+    xlab="datetime")
+           }
+    )
+
+## Saving to file
+dev.copy(png, file="plot4.png", 
+height=480, 
+width=480,
+units = "px", 
+bg = "transparent")
 dev.off()
