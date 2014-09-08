532ac1f7407974835cfb25c5899f540b91606c36
Rafael Resendiz Ramirez <rafaresend@gmail.com>
2014-09-07 19:46:13 -0700

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -1,15 +1,21 @@
-source("load_data.R")
-png(filename = "plot3.png", 
-    width = 480, height = 480,
-    units = "px", bg = "transparent")
-plot(DateTime, Sub_metering_1, 
-     type = "l",
-     col = "black",
-     xlab = "", ylab = "Energy sub metering")
-lines(DateTime, Sub_metering_2, col = "red")
-lines(DateTime, Sub_metering_3, col = "blue")
-legend("topright", 
-       col = c("black", "red", "blue"),
-       c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
-       lwd = 1)
+## Plot 3
+source("general_ploting.R")
+with(eda2, {
+    plot(Sub_metering_1~Datetime, type="l",
+    ylab="Global Active Power (kilowatts)", 
+    xlab="")
+    lines(Sub_metering_2~Datetime,col='Red')
+    lines(Sub_metering_3~Datetime,col='Blue')
+})
+legend("topright", col=c("black", "red", "blue"), 
+lty=1, 
+lwd=2, 
+legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+
+## Saving to file
+dev.copy(png, file="plot3.png", 
+  height=480, 
+  width=480,
+  units = "px", 
+  bg = "transparent")
 dev.off()
