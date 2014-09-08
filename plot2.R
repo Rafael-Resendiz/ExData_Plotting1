eacbb8af26ba90dd18d72fbf7273d1c1d399c032
Rafael Resendiz Ramirez <rafaresend@gmail.com>
2014-09-07 19:17:57 -0700

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -1,9 +1,9 @@
 source("general_ploting.R")
-png(filename = "plot2.png", 
-     width = 480, height = 480,
-     units = "px", bg = "transparent")
-plot(DateTime, Global_active_power, 
-     type = "l",
-     xlab = "",
-     ylab = "Global Active Power (kilowatts)")
-dev.off()
+ plot(eda2$Global_active_power~eda2$Datetime, 
+     type="l", 
+     ylab="Global Active Power (kilowatts)", 
+     xlab="")
+ dev.copy(png, file="plot2f.png", 
+     height=480, 
+     width=480)
+ dev.off()
